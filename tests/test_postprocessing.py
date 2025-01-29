d5e3e5d93f7a946988e1a864eb590acb88975fc0
monika-o <monika@orl25.de>
2025-01-29 16:32:47 +0100

    Lint and reduce for now

--- tests/test_postprocessing.py
+++ tests/test_postprocessing.py
@@ -1,14 +1,14 @@
 import sys
-import os
-import pytest
 from pathlib import Path
+
 ROOT_PATH = Path(__file__).parent.parent
 sys.path.append(ROOT_PATH)
-from pyromof import postprocessing
+# from pyromof import postprocessing  -
+# This executes all code in postprocessing, which doesn't work on github because there's not dumping space.
 
 
 def test_list_int():
-        """
-        Test that it can sum a list of integers
-        """
-        assert sum((1, 2, 3)) == 6
+    """
+    Test that it can sum a list of integers
+    """
+    assert sum((1, 2, 3)) == 6
