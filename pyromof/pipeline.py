00f21fd39e298009447e226b0ab865f8e172daf5
Monika Orlowski <monika.orlowski@ioew.de>
2026-02-20 10:38:05 +0100

    Add plotting to pipeline

--- pyromof/pipeline.py
+++ pyromof/pipeline.py
@@ -5,16 +5,24 @@ from pathlib import Path
 
 pyromof = Path(__file__).parent
 
-# Run first script
+# Run optimize
 script1 = os.path.join(pyromof, "optimize.py")
 result1 = subprocess.run([sys.executable, script1])
 if result1.returncode != 0:
     print("optimize.py failed. Aborting.")
     sys.exit(result1.returncode)
 
-# Run second script
+# Run postprocessing
 script2 = os.path.join(pyromof, "postprocessing.py")
 result2 = subprocess.run([sys.executable, script2])
 if result2.returncode != 0:
     print("postprocessing.py failed.")
     sys.exit(result2.returncode)
+
+
+# Run plotting
+script3 = os.path.join(pyromof, "plotting.py")
+result3 = subprocess.run([sys.executable, script3])
+if result3.returncode != 0:
+    print("plotting.py failed.")
+    sys.exit(result3.returncode)
