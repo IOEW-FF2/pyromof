cfd17c7a726d265c5db05778f628608d8bc2410a
Max4375 <klimabe@web.de>
2026-03-12 11:24:00 +0100

    Create plots for power_load_duration_curve and biomass_load_duration_curve based on functions

--- pyromof/load_duration_curve.py
+++ pyromof/load_duration_curve.py
@@ -1,34 +1,80 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-# define the path to the used data in results folder
 
-daten = ("./results/stromflex_h2/results/sequences.csv")
+def get_data_csv(
+          file_path: str,
+          separator: str,
+          parse_dates: bool, 
+          target_column: str,
+          
+):
+    
+    data = pd.read_csv(file_path, sep=separator, parse_dates=parse_dates)
+    return data[target_column]
 
 
-# define a function to sort the 'b_electricity to electricity_grid' column in descending order and return the sorted column as a pandas Series called sorted_column
-def load_duration_curve(daten):
-    print("Pfad:", daten)
-    print("Existiert:", daten.exists())
-    df = pd.read_csv(daten, index_col=0, parse_dates=True, sep=";")
-    print("Spalten:", df.columns)
-    sorted_column = df['b_electricity to electricity_grid'].sort_values(ascending=False)
+def sort_values_descending(column):
+    sorted_column = column.sort_values(ascending=False)
     return sorted_column
 
-sorted_load_curve = load_duration_curve(daten)
-print(sorted_load_curve.head())
+
+def plot_load_duration_curve(sorted_column, xlabel, ylabel, title, save_path):
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(len(sorted_column)), sorted_column.values)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.grid(True)
+    plt.savefig(save_path)
+    plt.show()
 
 
-# Plot the sorted_load_curve of the 'b_electricity to electricity_grid' column
-
-plt.figure(figsize=(10, 6))
-plt.plot(range(len(sorted_load_curve)), sorted_load_curve.values)
-plt.xlabel('Hours')
-plt.ylabel('Injected Energy')
-plt.title('Load duration curve')
-plt.grid(True)
 
 
-# save the plot in the results folder and show plot
+# receive data for power and biomass load
 
-plt.savefig('./results/stromflex_h2/results/load_duration_curve.png')
-plt.show()
+power_data = get_data_csv(
+    file_path="./results/stromflex_h2/results/sequences.csv",
+    separator=";",
+    parse_dates=True,
+    target_column="b_electricity to electricity_grid"
+)
+biomass_data = get_data_csv(
+    file_path="./results/stromflex_h2/results/sequences.csv",
+    separator=";",
+    parse_dates=True,
+    target_column="b_biomass_dry to pyrolysis"
+)
+
+
+# sort the data in descending order
+
+descending_power_data = sort_values_descending(power_data)
+descending_biomass_data = sort_values_descending(biomass_data)
+
+
+# plot the load duration curves for power and biomass load
+
+power_load_duration_curve = plot_load_duration_curve(
+    sorted_column=descending_power_data,
+    xlabel='Hours',
+    ylabel='Injected Energy',
+    title='Load duration curve - Power',
+    save_path='./results/stromflex_h2/results/load_duration_curve_power.png'
+)
+biomass_duration_curve = plot_load_duration_curve(
+    sorted_column=descending_biomass_data,
+    xlabel='Hours',
+    ylabel='Injected Biomass',
+    title='Load duration curve - Biomass',
+    save_path='./results/stromflex_h2/results/load_duration_curve_biomass.png'
+)
+
+
+power_load_duration_curve
+biomass_duration_curve
+
+
+exit()
