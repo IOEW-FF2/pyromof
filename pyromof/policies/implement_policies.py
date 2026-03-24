870871d8523c50f83fce2474ca2ae35afecc1624
Max4375 <klimabe@web.de>
2026-03-24 18:18:46 +0100

    Fix feed_in_policy functions

--- pyromof/policies/implement_policies.py
+++ pyromof/policies/implement_policies.py
@@ -1,16 +1,12 @@
 import pandas as pd
 
-from pyromof.policies.sliding_premium import (
-    feed_in_payment_cent_per_kWh,
-    receive_and_refine_electricity_price_data,
-    receive_base_value_and_lower_threshold,
-)
-
 
 def fixed_premium_policy(sink: pd.DataFrame, policies: pd.DataFrame, scenario: str) -> pd.DataFrame:
-    feed_in_premium = -policies.loc[
-        policies["policy"] == "Fixed feed-in remuneration", "value 1"
-    ].values[0]
+    feed_in_premium = (
+        -1
+        / 100
+        * policies.loc[policies["policy"] == "Fixed feed-in remuneration", "value 1"].values[0]
+    )
     activate_status = policies.loc[
         policies["policy"] == "Fixed feed-in remuneration", "activate"
     ].values[0]
@@ -23,22 +19,37 @@ def fixed_premium_policy(sink: pd.DataFrame, policies: pd.DataFrame, scenario: s
 
 
 def sliding_premium_policy(
-    sink: pd.DataFrame, policies: pd.DataFrame, scenario: str
-) -> pd.DataFrame:
-    base_value, lower_threshold = receive_base_value_and_lower_threshold(scenario)
-    electricity_price_cent_per_kWh = receive_and_refine_electricity_price_data()
-    sliding_feed_in_premium = feed_in_payment_cent_per_kWh(
-        electricity_price_cent_per_kWh, base_value, lower_threshold
+    sink: pd.DataFrame, policies: pd.DataFrame, profiles: pd.DataFrame, scenario: str
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+
+    base_value = (
+        -1 / 100 * policies.loc[policies["policy"] == "Sliding premium", "value 1"].values[0]
     )
+    lower_threshold = (
+        -1 / 100 * policies.loc[policies["policy"] == "Sliding premium", "value 2"].values[0]
+    )
+    electricity_price = profiles["profile_electricity_remuneration"].fillna(base_value)
     activate_status = policies.loc[policies["policy"] == "Sliding premium", "activate"].values[0]
 
+    feed_in_payment_euro_per_kWh = []
+
+    for i in range(len(electricity_price)):
+        if electricity_price.iloc[i] >= base_value and electricity_price.iloc[i] <= lower_threshold:
+            feed_in_payment_euro_per_kWh.append(base_value)
+
+        elif (
+            electricity_price.iloc[i] >= lower_threshold or electricity_price.iloc[i] <= base_value
+        ):
+            feed_in_payment_euro_per_kWh.append(electricity_price.iloc[i])
+
     if activate_status == "x":
+        profiles["sliding_premium_profile"] = feed_in_payment_euro_per_kWh
         sink.loc[
             (sink["label"] == "electricity_grid") & (sink["scenario"] == scenario),
             "variable_costs",
-        ] = sliding_feed_in_premium
+        ] = "sliding_premium_profile"
 
-    return sink
+    return sink, profiles
 
 
 def fix_investment_subsidy_policy(converters: pd.DataFrame, policies: pd.DataFrame) -> pd.DataFrame:
@@ -76,7 +87,11 @@ def percentage_investment_subsidy_policy(
 
 
 def redefine_sink_and_converter_for_policies(
-    sink: pd.DataFrame, converters: pd.DataFrame, policies: pd.DataFrame, scenario: str
+    sink: pd.DataFrame,
+    converters: pd.DataFrame,
+    policies: pd.DataFrame,
+    profiles: pd.DataFrame,
+    scenario: str,
 ) -> pd.DataFrame:
 
     if (
@@ -98,8 +113,8 @@ def redefine_sink_and_converter_for_policies(
         )
     else:
         sink = fixed_premium_policy(sink, policies, scenario)
-        sink = sliding_premium_policy(sink, policies, scenario)
+        sink, profiles = sliding_premium_policy(sink, policies, profiles, scenario)
         converters = fix_investment_subsidy_policy(converters, policies)
         converters = percentage_investment_subsidy_policy(converters, policies)
 
-    return sink, converters
+    return sink, converters, profiles
