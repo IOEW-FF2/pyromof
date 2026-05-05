e70ed4717ce6ed62f54e60e788370be5056819ea
Max4375 <klimabe@web.de>
2026-05-05 11:08:27 +0200

    Add postprocessing for capex subsidies

--- pyromof/policies/postprocess_policies_functions.py
+++ pyromof/policies/postprocess_policies_functions.py
@@ -43,3 +43,28 @@ def add_sums_to_log_file(
     revenue_sums.to_csv(
         os.path.join(results_dir, "electricity_revenue_data.csv"), index=False, sep=";"
     )
+
+
+def receive_capex_data(scenario, data):
+
+    policies = data["policies"]
+    converters = data["converters"]
+
+    yearly_biochar_output = (
+        1
+        / 1000
+        * pd.read_csv(
+            f"./results/{scenario}/results/sequences.csv", sep=";", index_col=0, parse_dates=True
+        )["b_biochar to biochar_market"].sum()
+    )
+    # Es wird ein Auslegungswert für den jährlichen Biochar output benötigt
+    # -> ist der 59,9 Wert der stündlich angelegte output Wert?
+    # aktuell wird dann nur Monatswert berechnet
+
+    # zweite Möglcihkeit:
+    yearly_biochar_output = (
+        8.76 * converters.loc[converters["label"] == "pyrolysis", "nominal_capacity"].values[0]
+    )
+    # 8.76 da stündlcher Wert * 8760 Stunden im Jahr und /1000 von kg zu t
+
+    return policies, converters, yearly_biochar_output
